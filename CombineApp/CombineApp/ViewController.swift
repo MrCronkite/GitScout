feb8c9f83c9535e90f31f9b409af2a02bd834336
vlad <vladshima99@gmail.com>
2026-07-18 18:29:56 +0300

    @Published

--- CombineApp/CombineApp/ViewController.swift
+++ CombineApp/CombineApp/ViewController.swift
@@ -8,45 +8,34 @@
 import UIKit
 import Combine
 
+class TemperatureMonitor {
+    @Published var celsius: Double = 0
+
+    private var cancellable = Set<AnyCancellable>()
+
+    init() {
+        $celsius
+            .map { $0 * 9 / 5 + 32 }
+            .filter { $0 > 100 }
+            .sink {
+                 print("Внимание! Слишком жарко: \($0)°F")
+            }
+            .store(in: &cancellable)
+
+    }
+}
+
 
 final class ViewController: UIViewController {
     
     @IBOutlet weak var photoImageView: UIImageView!
 
-
-    let publisher = Just(42)
-    var subscriptions = Set<AnyCancellable>()
-    let subject = PassthroughSubject<Int, Never>()
+    let temp = TemperatureMonitor()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let future = futureIncrement(integer: 1, delay: 3)
+        temp.celsius = 40 
 
-        future.sink {
-            print($0)
-        } receiveValue: {
-            print($0)
-        }
-        .store(in: &subscriptions)
-
-
-        subject.send(1)
-
-        let cancellable = subject.sink { value in
-            print("Получено: \(value)")
-        }
-
-        subject.send(2)
-        subject.send(3)
-
-    }
-
-    func futureIncrement(integer: Int, delay: TimeInterval) -> Future<Int, Never> {
-        Future { promise in
-            DispatchQueue.global().asyncAfter(deadline: .now() + delay) {
-                promise(.success(integer + 1))
-            }
-        }
     }
 }
