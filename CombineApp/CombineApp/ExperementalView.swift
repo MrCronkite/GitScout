cba9eac5f1c590a427724d9f338b2b69966b5889
vlad <vladshima99@gmail.com>
2026-07-27 13:45:49 +0300

    AsyncStream

--- CombineApp/CombineApp/ExperementalView.swift
+++ CombineApp/CombineApp/ExperementalView.swift
@@ -7,10 +7,7 @@
 
 import UIKit
 
-struct User: Decodable {
-    let id: Int
-    let name: String
-}
+struct TimeoutError: Error {}
 
 actor ImageCache {
     private var cache: [String: UIImage] = [:]
@@ -40,7 +37,7 @@ actor ImageCache {
         loadingImages[key] = task
 
         defer { loadingImages[key] = nil }
-        
+
         let valueImage = try await task.value
         cache[key] = valueImage
         return valueImage
@@ -52,52 +49,45 @@ final class ExperementalView {
     init() {}
 
     func start() async {
-        let task = Task {
-            do {
-                let users = try await fetchUsers(ids: ["1", "2", "3", "4", "5"])
-                print(users)
-            } catch is CancellationError {
-                print("cancelation")
-            } catch {
-                print(error.localizedDescription)
-            }
+        do {
+            try await consumeScores()
+        } catch is TimeoutError {
+            print("TimeoutError")
+        } catch {
+            print("error")
         }
-
-        try? await Task.sleep(for: .milliseconds(500))
-
-        task.cancel()
     }
 
-    func fetchUsers(ids: [String]) async throws -> [User] {
+    func consumeScores() async throws {
+        var iterator = liveScores().makeAsyncIterator()
 
-        var users = Array<User?>(repeating: nil, count: ids.count)
-
-        try await withThrowingTaskGroup(of: (Int, User).self) { [weak self] group in
-            guard let self else { throw CancellationError() }
-
-            for (index, id) in ids.enumerated() {
+        while true {
+            let next = try await withThrowingTaskGroup(of: Int?.self) { group -> Int? in
+                group.addTask { await iterator.next() }
                 group.addTask {
-                    let user = try await self.fetchUser(id)
-                    return (index, user)
+                    try await Task.sleep(for: .seconds(5))
+                    throw TimeoutError()
                 }
+
+                defer { group.cancelAll() }
+                return try await group.next() ?? nil
+
             }
 
-            for try await (index, user) in group {
-                users[index] = user
+            guard let score = next else { break }
+            print("Score:", score)
+        }
+    }
+
+    func liveScores() -> AsyncStream<Int> {
+        AsyncStream { continuation in
+            let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                continuation.yield(Int.random(in: 0...100))
+            }
+            continuation.onTermination = { _ in
+                timer.invalidate()
             }
         }
-
-        return users.compactMap { $0 }
-
     }
 
-    func fetchUser(_ id: String) async throws -> User {
-        try Task.checkCancellation()
-
-        try await Task.sleep(for: .milliseconds(500))
-
-        try Task.checkCancellation()
-
-        return User(id: Int(id) ?? 0, name: "nil")
-    }
 }
